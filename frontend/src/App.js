8d7f97eb2e96220d6d1e7f391aa6718bb3f58b6c
Terez <69874794+training6@users.noreply.github.com>
2021-04-21 04:31:22 +0200

    push

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Home from './pages/Pages';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route path='/'>
+          <Home/>
+        </Route>
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
